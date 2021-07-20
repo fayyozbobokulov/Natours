2e4d22fd0167be2b6d2357a1bae1ccdef70fb73f
fayyozbobokulov <b.fayyoz2000@gmail.com>
2021-07-21 01:26:57 +0500

    Some business logic is added to imporve the usability of the app

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,12 +7,15 @@ const {
   deleteTour,
   aliasTopTours,
   getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 // router.param('id', checkId);
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+
+router.route('/monthly-plan').get(getMonthlyPlan);
 
 router.route('/tour-stats').get(getTourStats);
 
